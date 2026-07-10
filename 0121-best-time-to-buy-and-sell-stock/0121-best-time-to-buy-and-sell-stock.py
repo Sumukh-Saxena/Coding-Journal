789f49733c46ebcdf4639cad269506f1864df7be
JokeRex <sumukh.saxen458@gmail.com>
2026-07-11 02:31:41 +0530

    Time: 64 ms (60.89%), Space: 18.9 MB (74.53%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        min_price = float('inf')
+        max_profit = 0
+        for i in prices:
+            if i < min_price:
+                min_price = i
+            max_profit = max(max_profit, i - min_price)
+        return max_profit
+        
