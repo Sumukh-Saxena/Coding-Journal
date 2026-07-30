60150e0c3fa342fcf9eb435b6b6381170be481ae
JokeRex <sumukh.saxen458@gmail.com>
2026-07-30 15:58:15 +0530

    Time: 0 ms (100%), Space: 8.3 MB (77.73%) - LeetHub

---
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+        int total_pushes = 0;
+        int n = word.length();
+        
+        for (int i = 0; i < n; ++i) {
+            total_pushes += (i / 8) + 1;
+        }
+        
+        return total_pushes;
+    }
+};
